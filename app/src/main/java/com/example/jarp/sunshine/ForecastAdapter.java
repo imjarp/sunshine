5341aa07da724881ae8162e1b3c710216d6783a7
unknown <ramirezp.jorge@gmail.com>
2014-08-01 10:42:42 -0500

    Add new assets implementing new utility

--- app/src/main/java/com/example/jarp/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/example/jarp/sunshine/ForecastAdapter.java
@@ -42,10 +42,24 @@ public class ForecastAdapter extends CursorAdapter {
     public void bindView(View view, Context context, Cursor cursor) {
 
         ViewHolder viewHolder = (ViewHolder) view.getTag();
+
+        int viewType = getItemViewType(cursor.getPosition());
+        switch (viewType)
+        {
+            case VIEW_TYPE_TODAY:
+                viewHolder.iconView.setImageResource(Utility.getArtResourceForWeatherCondition(
+                        cursor.getInt(ForecastFragment.COL_WEATHER_CONDITION_ID)));
+                break;
+            case VIEW_TYPE_FUTURE_DAY:
+                // Get weather icon
+                viewHolder.iconView.setImageResource(Utility.getIconResourceForWeatherCondition(
+                        cursor.getInt(ForecastFragment.COL_WEATHER_CONDITION_ID)));
+                break;
+        }
         // Read weather icon ID from cursor
         int weatherId = cursor.getInt(ForecastFragment.COL_WEATHER_ID);
         // Use placeholder image for now
-        viewHolder.iconView.setImageResource(R.drawable.ic_launcher);
+
 
         /*ImageView iconView = (ImageView) view.findViewById(R.id.list_item_icon);
         iconView.setImageResource(R.drawable.ic_launcher);*/
